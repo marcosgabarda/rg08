b1da97e1f36e190429d5e3bfb1aeeeea6d8900c6
cyberslas <cyberslas@b261f8a2-cb4b-0410-b387-5b5ceaa8bf5d>
2008-04-28 09:09:04 +0000

    Codigo original

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "huffman.h"
 
 /**
- * Hola
  * Módulo que lee un fichero de texto, y calcula la frecuencia de aparición de 
  * cada símbolo.
  * \author Iván Rodriguez Sastre & Marcos Gabarda Inat
