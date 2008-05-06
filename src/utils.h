775cdc565da527f31deedb1855fd5b718c618946
cyberslas <cyberslas@b261f8a2-cb4b-0410-b387-5b5ceaa8bf5d>
2008-05-06 15:51:13 +0000

    Ya comprime!

--- src/utils.h
+++ src/utils.h
@@ -2,10 +2,13 @@
 #define _UTILS_
 
 #include "types.h"
-
+#include "node.h"
 
 FMN leerFuente(string sFile);
-
-char creaOcteto (string sCodigo);
+char creaOcteto (string sCodigo, int &nCeros);
+string recuperaOcteto(char cCodigo, int nCeros);
+void comprimeFichero(string sFile, string sFileOut = "");
+void descomprimeFichero(string sFile);
+void serializaArbol(string &sSecuencia, Node *Arbol);
 
 #endif
