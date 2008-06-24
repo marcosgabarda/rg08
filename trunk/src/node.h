734d63934d891d029549b6383dd9844775283b20
irodrisa <irodrisa@b261f8a2-cb4b-0410-b387-5b5ceaa8bf5d>
2008-06-24 16:10:13 +0000

    Documentación de utils.cpp y node.cpp/.h

--- trunk/src/node.h
+++ trunk/src/node.h
@@ -1,17 +1,73 @@
 #ifndef _NODE_
 #define _NODE_
 
+/**
+ * \class Node
+ *
+ * Clase que implementa la representaci&oacute;n de un Nodo del &aacute;rbol
+ * de codificaci&oacute;n Huffman.
+ *
+ * Se usan m&eacute;todos para establecer y recuperar la informaci&oacute;n 
+ * de los distintos atributos del nodo.
+ *
+ * \date Julio 2008
+ * \author Iv&aacute;n Rodr&iacute;guez Sastre
+ * \author Marcos Gabarda Inat
+ *
+ */
+
 class Node {
  private:
+
+  /**
+   * \var m_cData 
+   * Caracter que guarda el s&iacute;mbolo almacenado en el nodo.
+   */
   unsigned char m_cData;
+
+  /**
+   * \var m_bIsLeaf 
+   * Booleano que define si un nodo es hoja o un nodo intermedio.
+   */
   bool m_bIsLeaf;
+
+  /**
+   * \var m_dProb
+   * Double que establece la probabilidad del s&iacute;mbolo.
+   */
   double m_dProb;
-  Node *m_pLeftSon; // Hijo 0
-  Node *m_pRightSon; // Hijo 1
+
+  /**
+   * \var m_pLeftSon
+   * Puntero al nodo descendiente por la rama izquierda (Hijo 0).
+   */
+  Node *m_pLeftSon;
+
+  /**
+   * \var m_pRightSon
+   * Puntero al nodo descendiente por la rama derecha (Hijo 1).
+   */
+  Node *m_pRightSon;
  public:
 
+  /**
+   * \var m_CodigoTemporal
+   * Pila que almacena el c&oacute;digo temporal obtenido al recorrer el
+   * &aacutebol;.
+   */
   stack<unsigned char> m_CodigoTemporal;
+
+  /**
+   * \var m_CodigoHoja
+   * Cadena que almacena el c&oacute;digo obtenido al llegar a una hoja.
+   */
   string m_CodigoHoja;
+
+  /**
+   * \var m_CodigoFinal
+   * Cadena que almacena el c&oacute;digo obtenido final tras recorrer el
+   * &aacute;rbol.
+   */
   COD m_CodigoFinal;
 
   Node ();
