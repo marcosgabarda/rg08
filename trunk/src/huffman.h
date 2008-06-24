6cacd75c9974ef01df52702864fa16eb798aa0f4
cyberslas <cyberslas@b261f8a2-cb4b-0410-b387-5b5ceaa8bf5d>
2008-06-24 16:08:24 +0000

    Documentado la clase Huffman, el fichero qsort.cpp y types.h. Añadido también el fichero y el directorio para generar la documentación.

--- trunk/src/huffman.h
+++ trunk/src/huffman.h
@@ -4,14 +4,32 @@
 #include "types.h"
 #include "node.h"
 
+/**
+ * \class Huffman
+ *
+ * Clase que modela la generación del &aacute;rbol de codificaci&oacute;n 
+ * Huffman, a partir de una fuente de memoria nula.
+ *
+ * \author Iv&aacute;n Rodr&iacute;guez Sastre
+ * \author Marcos Gabarda Inat
+ */
 class Huffman {
+
  private:
+
+  /**
+   * \var m_vFuente
+   * Vector de sub-&aacute;rboles de codificaci&oacute;n Huffman
+   */
   vector<Node *> m_vFuente;
+
  public:
+
   Huffman (FMN origen);
   void creaHuffman ();
   COD generaCodigo();
   Node *getTree();
+
 };
 
 #endif
